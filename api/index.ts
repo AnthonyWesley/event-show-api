56a6c7432ecc53a0403700abb8b80e73f4ba3ecf
Anthony Wesley <awl-7@live.com>
2025-05-22 17:25:04 -0300

    0.0.1

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,10 @@
 import { ApiExpress } from "../src/infra/api/express/api.express";
 import { routes } from "../src/presentation/routes/Routes";
-import serverless from "serverless-http";
 
-// Cria a instância
-const api = ApiExpress.create(routes);
+function main() {
+  const api = ApiExpress.create(routes);
+  const port = 8000;
+  api.start(port);
+}
 
-// Exporta como handler para Vercel
-const app = api.getApp(); // você vai adicionar esse método abaixo
-export const handler = serverless(app); // Vercel usa isso
+main();
