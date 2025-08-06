72b35017ad2aae587ee898e48211ef2444da1571
Anthony Wesley <awl-7@live.com>
2025-08-06 19:37:20 -0300

    vercel url para o cors

--- src/shared/utils/IdGenerator.ts
+++ src/shared/utils/IdGenerator.ts
@@ -1,7 +1,16 @@
-import { customAlphabet } from "nanoid";
 import { ulid } from "ulid";
 
 export const generateId = (): string => ulid();
 
-export const generateInviteCode = () =>
-  customAlphabet("ABCDEFGHJKLMNPQRSTUVWXYZ123456789", 6);
+export const generateInviteCode = (): string => {
+  const alphabet = "ABCDEFGHJKLMNPQRSTUVWXYZ123456789";
+  const length = 6;
+  let code = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * alphabet.length);
+    code += alphabet[randomIndex];
+  }
+
+  return code;
+};
